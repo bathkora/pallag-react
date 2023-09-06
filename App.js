e73a77bfc22f6f3e7b5d15b0292a2ab380c84ed3
bathkora <mukesh.bathkora@gmail.com>
2023-09-06 11:56:50 +0530

    changes in packages

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
 const parent = React.createElement("div", {id:"parent"}, [
     React.createElement("div", {id:"child"}, [
         React.createElement("h1", {}, "I'm an h1 tag"),
@@ -9,5 +14,5 @@ const parent = React.createElement("div", {id:"parent"}, [
 ]),
 ]);
              
-        const root = ReactDOM.createRoot(document.getElementById("parent"));
+        const root = ReactDOM.createRoot(document.getElementById("root"));
         root.render(parent);
